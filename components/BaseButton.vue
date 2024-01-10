2b7111cdcbf3e86269fe7915104c5beb0b85f07b
Emmanuel Martin <helloiamemmanuel@gmail.com>
2024-01-10 21:24:29 +0530

    feat(landing): initial commit

---
+++ components/BaseButton.vue
@@ -0,0 +1,28 @@
+<template>
+    <component
+        :is="to ? 'nuxt-link' : href ? 'a' : 'button'"
+        :href="href"
+        class="w-4/12 flex items-center justify-center rounded bg-white/90 py-3 text-black hover:bg-white"
+    >
+        <slot name="icon" />
+        <slot name="default" />
+    </component>
+</template>
+
+<script>
+export default {
+    props: {
+        to: {
+            type: String,
+            default: null
+        },
+        href: {
+            type: String,
+            default: null
+        }
+    }
+};
+</script>
+
+<style>
+</style>
